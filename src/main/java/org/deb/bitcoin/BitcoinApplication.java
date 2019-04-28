a44fbe7b79b807c8473505e7a32299399f1988a6
Debmalya Jash <debmalya.jash@gmail.com>
2019-04-28 18:27:52 +0800

    Update BitcoinApplication.java

--- src/main/java/org/deb/bitcoin/BitcoinApplication.java
+++ src/main/java/org/deb/bitcoin/BitcoinApplication.java
@@ -1,22 +1,38 @@
 package org.deb.bitcoin;
 
+import org.deb.bitcoin.json.processor.BitcoinDataProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @ComponentScan
-public class BitcoinApplication {
+public class BitcoinApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BitcoinApplication.class, args);
-	}
+    @Autowired
+    BitcoinDataProcessor bitcoinDataProcessor;
 
-	@RequestMapping("/")
-	public String doIt() {
-		return "classpath:resources/templates/bitcoin";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String doIt() {
+        return "classpath:resources/templates/bitcoin";
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("https://www.coinbase.com/api/v2/prices/BTC-USD/historic?period=all", String.class);
+        bitcoinDataProcessor.processBitcoinData(responseEntity.getBody());
+    }
 }
